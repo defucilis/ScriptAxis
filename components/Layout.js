e96283485c445b658a021ddbb4ed290ad385b20b
Defucilis <defucilis@gmail.com>
2020-12-05 18:58:57 +1100

    feat: Add favicon

--- components/Layout.js
+++ components/Layout.js
@@ -1,9 +1,13 @@
+import Head from 'next/head'
 import Header from './Header'
 import Footer from './Footer'
 
 const Layout = ({children, page}) => {
     return (
         <div>
+            <Head>
+                <link rel="shortcut icon" href="/favicon.ico" />
+            </Head>
             <Header page={page}/>
             <div className="content container">
                 {children}
