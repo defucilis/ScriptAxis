a5a2913ea8bf8658154f5946e2f46c560cc6c154
Defucilis <defucilis@gmail.com>
2020-12-06 18:12:05 +1100

    feat: Add page loading animation thing

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,30 @@
 import firebase from '../utilities/Firebase'
 import "@yaireo/tagify/dist/tagify.css"
 import './index.css'
+import Router from "next/router";
+import {useState, useEffect} from 'react'
 
 const App = ({Component, pageProps}) => {
-    return <Component {...pageProps} />
+    const [loading, setLoading] = useState(false);
+    useEffect(() => {
+        const startLoad = () => setLoading(true);
+        const endLoad = () => setLoading(false);
+        Router.events.on("routeChangeStart", startLoad);
+        Router.events.on("routeChangeComplete", endLoad);
+        Router.events.on("routeChangeError", endLoad);
+        return () => {
+            Router.events.off("routeChangeStart", startLoad);
+            Router.events.off("routeChangeComplete", endLoad);
+            Router.events.off("routeChangeError", endLoad);
+        }
+    }, [])
+    return (
+        <>
+            <div className={`loader top ${loading ? "loadingtop" : "notloadingtop"}`}></div>
+            <div className={`loader bottom ${loading ? "loadingbottom" : "notloadingbottom"}`}></div>
+            <Component {...pageProps} />
+        </>
+    )
 }
 
 export default App;
