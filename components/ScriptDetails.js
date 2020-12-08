99fb0378942d0c2bf9886a4f77f6116ead495c69
Defucilis <defucilis@gmail.com>
2020-12-08 16:55:35 +1100

    feat: Add automatic video embedding to script details

--- components/ScriptDetails.js
+++ components/ScriptDetails.js
@@ -7,7 +7,69 @@ import { FaRegEye } from 'react-icons/fa'
 import ReactMarkdown from 'react-markdown'
 import moment from 'moment'
 
+const getEmbed = url => {
+    let iframeLink = "";
+    if(!url) return null;
+    if(url.includes("pornhub.com")) {
+        const pieces = url.split("?");
+        console.log(pieces);
+        if(pieces.length < 2) return null;
+        const params = new URLSearchParams("?" + pieces[1])
+        const viewkey = params.get("viewkey");
+        if(!viewkey) return null;
+
+        iframeLink = `https://www.pornhub.com/embed/${viewkey}`;
+    } else if(url.includes("xhamster.com")) {
+        const pieces = url.split("-");
+        if(pieces.length < 2) return null;
+        iframeLink = `https://www.xhamster.com/embed/${pieces[pieces.length - 1].split("?")[0]}`;
+    } else if(url.includes("spankbang.com")) {
+        const pieces = url.replace("http://", "").replace("https://").split("/");
+        if(pieces.length < 2) return null;
+        iframeLink = `https://www.spankbang.com/${pieces[1]}/embed`;
+    } else if(url.includes("xvideos.com")) {
+        const pieces = url.replace("http://", "").replace("https://").split("/");
+        if(pieces.length < 2) return null;
+        iframeLink = `https://www.xvideos.com/embedframe/${pieces[1].replace("video", "")}`;
+    } else if(url.includes("redtube.com")) {
+        const pieces = url.replace("http://", "").replace("https://").split("/");
+        if(pieces.length < 2) return null;
+        iframeLink = `https://embed.redtube.com?id=${pieces[1]}`;
+    } else if(url.includes("porntube.com")) {
+        let pieces = url.replace("http://", "").replace("https://").split("/");
+        if(pieces.length < 2) return null;
+        pieces = pieces[pieces.length - 1].split("?")[0].split("_");
+        if(pieces.length < 2) return null;
+        iframeLink = `https://www.porntube.com/embed/${pieces[1]}`;
+    } else if(url.includes("you-porn.com")) {
+        iframeLink = url.replace("you-porn.com/watch/", "you-porn.com/embed/");
+    } else if(url.includes("tube8.com")) {
+        iframeLink = url.replace("tube8.com", "tube8.com/embed");
+    } else if(url.includes("youjizz.com")) {
+        let pieces = url.replace("http://", "").replace("https://").split("/");
+        if(pieces.length < 2) return null;
+        pieces = pieces[pieces.length - 1].split("-");
+        iframeLink = `https://www.youjizz.com/videos/embed/${pieces[pieces.length - 1].replace(".html", "").split("?")[0]}`;
+    } else if(url.includes("eporner.com")) {
+        let pieces = url.replace("http://", "").replace("https://").split("/");
+        if(pieces.length < 3) return null;
+        iframeLink = `https://www.eporner.com/embed/${pieces[2]}`;
+    } else if(url.includes("tnaflix.com")) {
+        let pieces = url.replace("http://", "").replace("https://").split("/");
+        if(pieces.length < 4) return null;
+        iframeLink = `https://player.tnaflix.com/video/${pieces[pieces.length - 1].replace("video", "").split("?")[0]}`
+    } else if(url.includes("homemoviestube.com")) {
+        let pieces = url.replace("http://", "").replace("https://").split("/");
+        if(pieces.length < 3) return null;
+        iframeLink = `https://www.homemoviestube.com/embed/${pieces[2]}`;
+    }
+    return iframeLink;
+}
+
 const ScriptDetails = ({script}) => {
+
+    //todo - we could show the thumbnail while waiting for the iframe to load...
+
     return (
         <div className={style.script}>
             <h1 className={style.name}>{script.name}</h1>
@@ -19,7 +81,10 @@ const ScriptDetails = ({script}) => {
             <p className={style.created}>{moment(script.created, "x").format("Do MMMM YYYY")}</p>
             <div className={style.sidebyside}>
                 <div className={style.imagewrapper}>
-                    <img src={script.thumbnail} />
+                    {getEmbed(script.streamingUrl) ? 
+                        <iframe src={getEmbed(script.streamingUrl)} width={1000} height={500} allowFullScreen onLoad={() => console.log("iFrame loaded!")} /> : 
+                        <img src={script.thumbnail} />
+                    }
                 </div>
                 <div className={style.details}>
                     <p className={style.category}>{!script.category ? "No Category" : (
