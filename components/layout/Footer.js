a7cbf620c71b20b1587b00f2a8e7f8260e6deefe
Defucilis <defucilis@gmail.com>
2021-02-18 11:57:29 +1100

    feat: Add version number to footer

--- components/layout/Footer.js
+++ components/layout/Footer.js
@@ -1,10 +1,13 @@
+import packageJson from 'package.json'
 import style from './Footer.module.css'
 
 const Footer = () => {
+    console.log(packageJson);
     return (
         <div className={style.footer}>
             <div className="container">
                 <p>&copy; ScriptAxis 2020</p>
+                <p>v{packageJson.version}</p>
             </div>
         </div>
     )
